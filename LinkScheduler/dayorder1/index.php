716e921b53b7e24d6abd1222002b5bb6facc87df
Revanth2494 <62490738+Revanth2494@users.noreply.github.com>
2021-11-27 18:52:28 +0530

    Add files via upload

---
+++ LinkScheduler/dayorder1/index.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="main.css">
+  <title>Link scheduler</title>
+</head>
+
+<body>
+  <div class="box">
+    <h2>DAYORDER 1</h2>
+    <form action="config.php">
+      <div>
+        <label for="">CLASS 1</label>
+        <input type="text" name="class1" required>
+      </div>
+      <div>
+        <label for="">CLASS 2</label>
+        <input type="text" name="class2" required>
+      </div>
+      <div>
+        <label for="">CLASS 3</label>
+        <input type="text" name="class3" required>
+      </div>
+      <div>
+        <label for="">CLASS 4</label>
+        <input type="text" name="class4" required>
+      </div>
+      <input type="submit" value="Save">
+    </form>
+  </div>
+</body>
+
+</html>
